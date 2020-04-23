9bc1261ac8ff6d5b266666f0edc0f20fdb74baa3
1devhall <devoughnh@gmail.com>
2020-04-23 12:25:27 -0400

    mvp delet button troubles

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,38 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
+import UpdateMovie from "./Movies/UpdateMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
+  
+  const [ movieCount, setMovieCount ] = useState(0);
+  const [ editCount, setEditCount ] = useState(0);
 
   const getMovieList = () => {
     axios
       .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
+      .then(res => {
+        setMovieList(res.data);
+        setMovieCount(res.data.length);
+      })
       .catch(err => console.log(err.response));
   };
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
+  const decreaseMovieCount = () => {
+    setMovieCount(movieCount - 1);
+  }
+  const handleEditCount = () => {
+    setEditCount(editCount + 1);
+  }
 
   useEffect(() => {
     getMovieList();
-  }, []);
+  }, [movieCount, editCount]);
 
   return (
     <>
@@ -33,7 +46,11 @@ const App = () => {
       </Route>
 
       <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
+        <Movie decreaseMovieCount={decreaseMovieCount} addToSavedList={addToSavedList} />
+      </Route>
+
+      <Route path='/update-movie/:id'>
+        <UpdateMovie handleEditCount={handleEditCount}/>
       </Route>
     </>
   );
